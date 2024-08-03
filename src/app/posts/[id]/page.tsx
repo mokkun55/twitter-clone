b673e6c4b229a0fa63c9776a834b46355f22e3d8
mokkun55 <mokkunpc@gmail.com>
2024-08-03 13:52:37 +0900

    ツイート詳細　スタイルだけ

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import Sidebar from "@/app/components/Sidebar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import FollowButton from "@/app/components/buttons/FollowButton";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import IosShareIcon from "@mui/icons-material/IosShare";
+import { useRecoilValue } from "recoil";
+import { loginUserProfile } from "@/app/globalState";
+import useLoginUser from "@/hooks/useLoginUser";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../../firebase";
 
 type Props = {
   params: {
@@ -16,9 +26,20 @@ type Props = {
 // TODO ここからやる
 
 const Page = ({ params }: Props) => {
-  const [isTweet, setIsTweet] = useState<boolean>(true);
   const postId = params.id;
   const router = useRouter();
+  const [user] = useAuthState(auth);
+
+  const [isTweet, setIsTweet] = useState<boolean>(true);
+
+  const userProfile = useRecoilValue(loginUserProfile);
+  const { getUserProfile } = useLoginUser();
+
+  useEffect(() => {
+    // データ取得
+    !userProfile && getUserProfile();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
 
   return (
     <div className="flex justify-center">
@@ -60,11 +81,89 @@ const Page = ({ params }: Props) => {
             <FollowButton />
           </div>
 
+          {/* 本文 */}
+          <div className="mt-2">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Molestiae, et saepe. Saepe, dicta. Nobis natus commodi corrupti
+              quo qui alias harum atque perferendis repellendus iste veniam
+              suscipit itaque minima eveniet numquam quod, dolore, provident
+              molestias? Deserunt aspernatur officia laboriosam corrupti.
+            </p>
+          </div>
+
+          {/* TODO 日付 */}
+          <div>
+            <p className="text-gray-500 text-sm py-4">
+              午後xx:xx・2024年8月2日
+            </p>
+          </div>
+
           {/* アイコンたち */}
-          <div className="flex items-center justify-between"></div>
+          <div className="text-gray-500 pl-[45px] w-full flex items-center *:transition *:duration-300 justify-between border-y">
+            <button
+              className="hover:text-blue-500 hover:bg-blue-100 p-2 rounded-full"
+              // onClick={clickReply}
+            >
+              <ChatBubbleOutlineIcon />
+            </button>
+
+            <button
+              className="hover:text-green-500 hover:bg-green-100 p-2 rounded-full"
+              // onClick={clickRetweet}
+            >
+              <RepeatIcon />
+            </button>
+
+            <button
+              className="hover:text-red-500 hover:bg-red-100 p-2 rounded-full"
+              // onClick={clickLike}
+            >
+              <FavoriteBorderIcon />
+            </button>
+
+            <button
+              className="hover:text-blue-500 hover:bg-blue-100 p-2 rounded-full"
+              // onClick={clickBookmark}
+            >
+              <BookmarkBorderIcon />
+            </button>
+
+            <button
+              className="hover:text-blue-500 hover:bg-blue-100 p-2 rounded-full"
+              // onClick={clickShare}
+            >
+              <IosShareIcon />
+            </button>
+          </div>
         </div>
 
-        {/* 返信をツイートしよう */}
+        {/* 返信欄 */}
+        <div className="flex m-2">
+          <Image
+            src={userProfile?.profileImg || "/noImg.jpg"}
+            alt="profile"
+            width={50}
+            height={50}
+            className="rounded-full w-[50px] h-[50px] mr-2"
+          />
+          <div className="mt-3 w-full h-fit ">
+            <textarea
+              className="resize-none outline-none w-full text-lg"
+              placeholder="いまどうしてる？"
+              // onChange={(e) => setPostText(e.target.value)}
+              // value={postText}
+            ></textarea>
+            <div className="flex">
+              <button
+                className="bg-blue-500 text-white py-2 px-4 rounded-full ml-auto"
+                // onClick={clickSendTweet}
+              >
+                返信
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="w-[10%] lg:w-[40%]"></div>
     </div>
